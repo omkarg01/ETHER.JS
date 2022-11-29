8f5784bf4baa6cbf42e5dc6ade585ea308ae4ee1
omkarg01 <omkargujja01@gmail.com>
2022-11-29 14:30:58 +0530

    comments for interactionSC

---
+++ interactionSC.js
@@ -0,0 +1,10 @@
+// get the contract address on which the SC has deployed
+// import provider from ethers and assign inufra ID
+
+// get wallet address
+// get ABI of deployed SC
+
+// perform read and write opeartions
+{
+
+}
