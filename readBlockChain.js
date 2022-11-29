46dbb4e15487011cef92033348af84164eda91bf
omkarg01 <omkargujja01@gmail.com>
2022-11-29 13:41:50 +0530

    initial commit

---
+++ readBlockChain.js
@@ -0,0 +1,13 @@
+// import provider from ethers and assign inufra ID
+
+
+// querryFrom ethereum blockChain
+{
+    // get recent block number
+
+    // get balance of any random address
+
+    // display it in ethers
+
+}
+
